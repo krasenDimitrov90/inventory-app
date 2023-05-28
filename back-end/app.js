39202b7bf8d4781a3047ce0338f0585e06c3cd3f
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-05-28 14:14:19 +0300

    [BE] init the server app

--- back-end/app.js
+++ back-end/app.js
@@ -1,1 +1,16 @@
-let a = 5;
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+    res.json({hello: 'World'});
+})
+
+app.listen(3000, () => {
+    console.log('Server is running, access on http://localhost:3000');
+})
